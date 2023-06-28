91636ebbd2ad1b114b073d9eb68e3b47d1a84217
jefferson pereira <jefferson.pereira76@gmail.com>
2023-06-28 14:48:00 -0300

    Criando os models Produto e Variação

--- produto/admin.py
+++ produto/admin.py
@@ -1,3 +1,17 @@
-from django.contrib import admin
+from django.contrib import admin            # type ignore
+from . import models
 
-# Register your models here.
+
+class VariacaoInline(admin.TabularInline):
+    model = models.Variacao
+    extra = 1
+
+
+class ProdutoAdmin(admin.ModelAdmin):
+    inlines = [
+        VariacaoInline
+    ]
+
+
+admin.site.register(models.Produto)
+admin.site.register(models.Variacao)
