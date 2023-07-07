10d684f4857d1b49434efd1c6193b84693c35349
jefferson pereira <jefferson.pereira76@gmail.com>
2023-07-07 10:45:59 -0300

    Lista e detalhe de pedidos para o usuário

--- pedido/views.py
+++ pedido/views.py
@@ -125,6 +125,16 @@ class SalvarPedido(View):
         )
 
 
-class Detalhe(View):
-    def get(self, *args, **kwargs):
-        return HttpResponse('Detalhe')
+class Detalhe(DispatchLoginRequiredMixin, DetailView):
+    model = Pedido
+    context_object_name = 'pedido'
+    template_name = 'pedido/detalhe.html'
+    pk_url_kwarg = 'pk'
+
+
+class Lista(DispatchLoginRequiredMixin, ListView):
+    model = Pedido
+    context_object_name = 'pedidos'
+    template_name = 'pedido/lista.html'
+    paginate_by = 10
+    ordering = ['-id']
